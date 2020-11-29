9ea3bb23ee005bef5d05cc2b6db739cb7c44a50e
Margarita Korolkova <mkorolkova311@gmail.com>
2020-11-29 18:08:59 +0300

    test3

--- Continious_integration/src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ Continious_integration/src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,7 @@ class StatisticsServiceTest {
   void findMaxTest() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long[] incomesInBillions = {2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
 
     long actual = service.findMax(incomesInBillions);
